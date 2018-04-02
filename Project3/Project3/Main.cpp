734096ec45f8a39005f7036b5812922ab640a9c8
adam <adamthewilliams@gmail.com>
2018-04-01 21:22:41 -0500

    cleaning up with more progress

--- Project3/Project3/Main.cpp
+++ Project3/Project3/Main.cpp
@@ -9,13 +9,23 @@
 #include<utility>
 using namespace std;
 
+typedef unordered_map<string, unordered_multimap<string, Spell*>> lookupDir;
+typedef unordered_multimap<string, Spell*> hashTable_Spell;
+typedef unordered_multimap<string, Spell*> hashTable_Player;
 
 struct Spell
 {
       string  name;
       string  sClass;
       string  type;
-      int     lvl;
+      string  lvl;
+};
+
+struct Player
+{
+      string name;
+      string plClass;
+      string maxLvl;
 };
 
 void printSpell(Spell* tmpSpell)
@@ -27,16 +37,38 @@ void printSpell(Spell* tmpSpell)
       return;
 }
 
+string printSpellByClass(hashTable_Spell* base)
+{
+      hashTable_Spell::iterator iter1 = base->begin();
+      while(iter1 != base->end())
+      { 
+            auto iter2 = base->equal_range(iter1->first);
+            for (;iter1 != iter2.second; ++iter1)
+            { 
+            
+            }
+      }
+}
+string printSpellByLvl(unordered_multimap<int, Spell*>* tmpMap, int lvl)
+{
+
+}
+
 int main()
 {
-      unordered_multimap<string, Spell*>  spNameLookup;
-      unordered_multimap<string, Spell*>  spClassLookup;
-      unordered_multimap<string, Spell*>  spTypeLookup;
-      unordered_multimap<int, Spell*>     spLvlLookup;
-      vector<Spell>                       spellList;
-      ifstream                            inFile;
-      string                              tmp, tmp2, tmp3;
-      int                                 tmpInt;
+      lookupDir        lookup;           //holds all the attribute indexes
+      hashTable_Spell  spNameIndex;      //holds pairs of spell names, and pointers to spell obj
+      hashTable_Spell  spClassIndex;     //holds pairs of spell classes, and pointers to spell obj
+      hashTable_Spell  spTypeIndex;      //holds pairs of spell types, and pointers to spell obj
+      hashTable_Spell  spLvlIndex;       //holds pairs of spell levels, and pointers to spell obj
+      hashTable_Player plNameIndex;      //holds pairs of player names, and pointers to player obj
+      hashTable_Player plClassIndex;     //holds pairs of player classes, and pointers to player obj
+      hashTable_Player plMaxLvlIndex;    //holds pairs of player max level, and pointers to player obj 
+      vector<Player>   playerList;       //vector of all the players, who can we searched by any index
+      vector<Spell>    spellList;        //vector of all the spells; all can be searched by spell index
+      ifstream         inFile;           //istream to import data files
+      string           tmp, tmp2, tmp3;  //temporary strings to capture file input
+
 
       inFile.open("data2.txt");
       if (inFile.fail())
@@ -47,17 +79,19 @@ int main()
       //pass by column headers
       inFile >> tmp >> tmp2 >> tmp3;
       //load spell names in from file
+      string tmpClass = "";
       while (!inFile.eof())
       {
+            string tmpName;
             Spell sp;
 
             inFile >> tmp;
             inFile >> tmp2;
-            inFile >> tmpInt;
+            inFile >> tmp3;
 
             sp.name   = tmp;
             sp.sClass = tmp2;
-            sp.lvl    = tmpInt;
+            sp.lvl    = tmp3;
             sp.type   = "";
 
             spellList.push_back(sp);
@@ -67,7 +101,7 @@ int main()
       //initialize lookup tables for each spell attribute
       vector<pair<string, Spell*>>*  names   = new vector<pair<string, Spell*>>;
       vector<pair<string, Spell*>>*  classes = new vector<pair<string, Spell*>>;
-      vector<pair<int, Spell*>>*     levels  = new vector<pair<int, Spell*>>;
+      vector<pair<string, Spell*>>*  levels  = new vector<pair<string, Spell*>>;
 
       //load names, classes, and levels into vectors alongside a ptr to the parent object
       for (auto iter : spellList)
@@ -79,7 +113,7 @@ int main()
             pair<string, Spell*> tmpPair2 = make_pair(iter.sClass, &spellList.at(i));
             classes->push_back(tmpPair2);
 
-            pair<int, Spell*> tmpPair3 = make_pair(iter.lvl, &spellList.at(i));
+            pair<string, Spell*> tmpPair3 = make_pair(iter.lvl, &spellList.at(i));
             levels->push_back(tmpPair3);
             i++;
       }
@@ -88,9 +122,13 @@ int main()
       sort(classes->begin(), classes->end());
       sort(levels->begin(), levels->end());
       //load vectors into hash tables
-      for (auto iter : *names   ) { spNameLookup.insert(iter);  } delete names;
-      for (auto iter : *classes ) { spClassLookup.insert(iter); } delete classes;
-      for (auto iter : *levels  ) { spLvlLookup.insert(iter);   } delete levels;
+      for (auto iter : *names   ) { lookup["spNameLookup"].insert(iter);  } delete names;
+      for (auto iter : *classes ) { lookup["spClassLookup"].insert(iter); } delete classes;
+      for (auto iter : *levels  ) { lookup["spLvlLookup"].insert(iter);   } delete levels;
+
+      lookup["spNameLookup"]  = spNameIndex;
+      lookup["spClassLookup"] = spClassIndex;
+      lookup["spLvlLookup"]   = spLvlIndex;
       /*
       sort(begin(spNameLookup), end(spNameLookup));
       sort(begin(spTypeLookup), end(spTypeLookup));
@@ -101,6 +139,10 @@ int main()
       {
             cout << "File failed to open, exiting program\n";
             exit(1);
+      }
+      while (!inFile.eof())
+      {
+
       }
       //pass by column headers 
       inFile >> tmp >> tmp2;
@@ -124,6 +166,7 @@ int main()
                   i++;
             }
       }
+      inFile.close();
       //create temporary vector and load types in
       vector<pair<string, Spell*>>* types = new vector<pair<string, Spell*>>;
       for (auto iter : spellList)
@@ -134,12 +177,20 @@ int main()
             ++i;
       }
       sort(types->begin(), types->end());
-      for (auto iter : *types) { spTypeLookup.insert(iter); } delete types;
+      for (auto iter : *types) { spTypeIndex.insert(iter); } delete types;
+      lookup["spTypeLookup"] = spTypeIndex;
+
+      inFile.open("data3.txt");
+      if (inFile.fail())
+      {
+            cout << "File failed to open. Exiting program...";
+            exit(1);
+      }
       char userResponse;
       do
       {
             string choice;
-            int intChoice;
+            //int intChoice;
             int menuChoice;
             cout << "1 - Search Spell By Name\n";
             cout << "2 - Search Spell By Class\n";
@@ -152,10 +203,10 @@ int main()
                   case 1:
                   {
                         cout << "\n\nEnter Spell Name: "; cin >> choice;
-                        auto iter2 = spNameLookup.find(choice);
-                        if (spNameLookup.count(choice) > 1)
+                        auto iter2 = spNameIndex.find(choice);
+                        if (spNameIndex.count(choice) > 1)
                         {
-                              auto iter = spNameLookup.equal_range(choice);
+                              auto iter = spNameIndex.equal_range(choice);
                               for (; iter2 != iter.second; ++iter2)
                               {
                                     printSpell(iter2->second);
@@ -167,10 +218,10 @@ int main()
                   case 2:
                   {
                         cout << "\n\nEnter Spell Class: "; cin >> choice;
-                        auto iter2 = spClassLookup.find(choice);
-                        if (spClassLookup.count(choice) > 1)
+                        auto iter2 = spClassIndex.find(choice);
+                        if (spClassIndex.count(choice) > 1)
                         {
-                              auto iter = spClassLookup.equal_range(choice);
+                              auto iter = spClassIndex.equal_range(choice);
                               for (; iter2 != iter.second; ++iter2)
                               {
                                     printSpell(iter2->second);
@@ -182,10 +233,10 @@ int main()
                   case 3:
                   {
                         cout << "\n\nEnter Spell Type: "; cin >> choice;
-                        auto iter2 = spTypeLookup.find(choice);
-                        if (spTypeLookup.count(choice) > 1)
+                        auto iter2 = spTypeIndex.find(choice);
+                        if (spTypeIndex.count(choice) > 1)
                         {
-                              auto iter = spTypeLookup.equal_range(choice);
+                              auto iter = spTypeIndex.equal_range(choice);
                               for (; iter2 != iter.second; ++iter2)
                               {
                                     printSpell(iter2->second);
@@ -196,11 +247,11 @@ int main()
                   }
                   case 4:
                   {
-                        cout << "\n\nEnter the Spell Level: "; cin >> intChoice;
-                        auto iter2 = spLvlLookup.find(intChoice);
-                        if (spClassLookup.count(choice) > 1)
+                        cout << "\n\nEnter the Spell Level: "; cin >> choice;
+                        auto iter2 = spLvlIndex.find(choice);
+                        if (spClassIndex.count(choice) > 1)
                         {
-                              auto iter = spLvlLookup.equal_range(intChoice);
+                              auto iter = spLvlIndex.equal_range(choice);
                               for (; iter2 != iter.second; ++iter2)
                               {
                                     printSpell(iter2->second);
